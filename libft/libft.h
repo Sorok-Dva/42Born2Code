a66be593b40e25d651c9c8d96d70b037d010614a
Llyam GARCIA <lgarcia@e2r5p17.42.fr>
2017-11-10 18:13:36 +0100

    Add prototypes

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lgarcia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 17:27:43 by lgarcia           #+#    #+#             */
-/*   Updated: 2017/11/10 17:27:52 by lgarcia          ###   ########.fr       */
+/*   Updated: 2017/11/10 18:12:15 by lgarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,16 @@
 # define MALLCHECK(x) if (!x) return (-1);
 # define IS_SPACE(x) (x == ' ' || x == '\t' || x == '\r' || x = '\f')
 
-void    ft_putchar(char c);
-void    ft_putchar_fd(char c, int fd);
-void    ft_putstr(char const *str);
-void    ft_putendl(char const *str);
-void    ft_putnbr(int nbr);
-void        ft_putnbr_fd(int n, int fd);
+void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr(char const *str);
+void	ft_putendl(char const *str);
+void	ft_putnbr(int nbr);
+void	ft_putnbr_fd(int n, int fd);
 
-size_t    ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strcpy(char *dst, const char *src);
+
+size_t	ft_strlen(const char *s);
 
 #endif
